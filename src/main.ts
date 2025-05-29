2283996eb2147d486b238601c8562b2d24b4b8bb
Dilan <dilan6465@gmail.com>
2025-05-29 02:52:25 -1000

    actualizacion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
+import * as crypto from 'crypto';  // Importa crypto primero
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Inyecta crypto en global si no existe
+  if (!(global as any).crypto) {
+    (global as any).crypto = crypto;
+  }
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
