51f8ef878558244d1be7ef4de8db748b51e967c7
Dilan <dilan6465@gmail.com>
2025-05-29 02:59:00 -1000

    ACTUALIZACION CRASHED

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,9 @@
+import * as crypto from 'crypto';
+
+if (!(global as any).crypto) {
+  (global as any).crypto = crypto;
+}
+
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
