5ce1c4c4dd003d0531f227959c3ae64858ba8cf8
Dilan <dilan6465@gmail.com>
2025-05-30 02:22:39 -1000

    Actualización: conexión con DATABASE_

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { TeamsModule } from './teams/teams.module';
 import { CategoriesModule } from './categories/categories.module';
 import { PlayersModule } from './players/players.module';
-
 
 @Module({
   imports: [
@@ -19,24 +21,25 @@ import { PlayersModule } from './players/players.module';
 
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        synchronize: true, 
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        autoLoadEntities: true,
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // Corregido
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
       }),
+      inject: [ConfigService],
     }),
 
     TeamsModule,
     CategoriesModule,
     PlayersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
 export class AppModule {}
-
-
