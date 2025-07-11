fc4f49ac621125fc0626511f1c67ab1c6b549a51
Dilan <dilan6465@gmail.com>
2025-07-10 21:28:02 -0500

    final

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,34 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
+  // Compatibilidad con dependencias que requieren global.crypto
   if (!(global as any).crypto) {
     (global as any).crypto = crypto;
   }
 
   const app = await NestFactory.create(AppModule);
 
+  // CORS configurado para frontend HTTPS desde NGINX
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://65.38.96.125:5173'],
+  origin: [
+    'https://resultados-deportivos-backend.desarrollo-software.xyz',
+    'https://desarrollo-software.xyz',
+    'http://localhost:5173', // solo si sigues probando localmente
+  ],
     credentials: true,
   });
 
+  // Validación global de DTOs
   app.useGlobalPipes(new ValidationPipe());
 
+  // Guards de autenticación y roles
   const reflector = app.get(Reflector);
   app.useGlobalGuards(
     new JwtAuthGuard(reflector),
     new RolesGuard(reflector)
   );
 
-  await app.listen(process.env.PORT || 3006, '0.0.0.0'); 
+  // Escucha desde todas las interfaces (VPS y proxy)
+  await app.listen(process.env.PORT || 3006, '0.0.0.0');
 }
 bootstrap();
