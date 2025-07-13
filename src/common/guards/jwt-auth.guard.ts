fc6758f266390fe5d6049a229aa898c6096b5818
Dilan <dilan6465@gmail.com>
2025-07-13 10:45:19 -0500

    verificacion fin

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,15 +1,28 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  Logger,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  private readonly logger = new Logger('JwtAuthGuard');
+
   constructor(private readonly reflector: Reflector) {
     super();
   }
 
   canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.get<boolean>('isPublic', context.getHandler());
-    return isPublic ? true : super.canActivate(context);
+
+    if (isPublic) {
+      this.logger.debug('🔓 Ruta pública detectada, omitiendo JWT');
+      return true;
+    }
+
+    this.logger.debug('🛡️ Protegiendo ruta con JWT');
+    return super.canActivate(context);
   }
 }
