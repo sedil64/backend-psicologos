03093b5c42e96217d9325729d9be04e0df97a1a5
Dilan <dilan6465@gmail.com>
2025-07-13 11:03:36 -0500

    arreglo auth.modul

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'TU_SECRETO_JWT',
+      secretOrKey: process.env.JWT_SECRET
     });
   }
 
