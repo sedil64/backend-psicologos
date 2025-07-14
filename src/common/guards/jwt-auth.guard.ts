0d857d752d65084db05b4302c8f18c97279cee16
Dilan <dilan6465@gmail.com>
2025-07-13 19:11:13 -0500

    ajuste

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,16 +1,5 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Reflector } from '@nestjs/core';
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super();
-  }
-
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.get<boolean>(IS_PUBLIC_KEY, context.getHandler());
-    return isPublic ? true : super.canActivate(context);
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
