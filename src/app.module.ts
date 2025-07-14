6a4fba2b99e4c6bc8ee772d3884b195f14c10d8c
Dilan <dilan6465@gmail.com>
2025-07-13 19:01:52 -0500

    cambio

--- src/app.module.ts
+++ src/app.module.ts
@@ -54,16 +54,7 @@ import { RolesGuard } from './common/guards/roles.guard';
   controllers: [AppController],
   providers: [
     AppService,
-
-    // ✅ Guards inyectados correctamente por NestJS
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
+    
   ],
 })
 export class AppModule {}
