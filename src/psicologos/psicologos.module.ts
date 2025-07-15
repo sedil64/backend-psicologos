05b677189196df81c383773f8551efa461f17b3c
sedil64 <dilan6465@gmail.com>
2025-07-14 20:29:18 -0500

    cambios cita

--- src/psicologos/psicologos.module.ts
+++ src/psicologos/psicologos.module.ts
@@ -1,19 +1,27 @@
 // src/psicologos/psicologos.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { PsicologosController } from './psicologos.controller';
-import { PsicologosService } from './psicologos.service';
-import { Psicologo } from './entities/psicologos.entity';
-import { Account } from '../auth/entities/account.entity';
-import { Cita } from '../citas/entities/citas.entity';
-import { Paciente } from '../pacientes/entities/paciente.entity';
+import { PsicologosService }    from './psicologos.service';
+
+import { Psicologo }    from './entities/psicologos.entity';
+import { Account }      from '../auth/entities/account.entity';
+import { Cita }         from '../citas/entities/citas.entity';
+import { Paciente }     from '../pacientes/entities/paciente.entity';
+
+import { AuthModule }             from '../auth/auth.module';
+import { CertificacionesModule }  from '../certificaciones/certificaciones.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Psicologo, Account, Cita, Paciente]),
-    // … otros módulos…
+
+    // IMPORTAR los módulos que proveen AuthService y CertificacionesService
+    AuthModule,
+    CertificacionesModule,
   ],
   controllers: [PsicologosController],
-  providers: [PsicologosService],
+  providers:   [PsicologosService],
 })
 export class PsicologosModule {}
