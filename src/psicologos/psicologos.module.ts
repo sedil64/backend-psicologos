54849fc5581eb4745c611859afd8fb592b68bcfe
sedil64 <dilan6465@gmail.com>
2025-07-19 09:46:57 -0500

    inclusion disponibilidad

--- src/psicologos/psicologos.module.ts
+++ src/psicologos/psicologos.module.ts
@@ -3,28 +3,33 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PsicologosController } from './psicologos.controller';
-import { PsicologosService }    from './psicologos.service';
+import { PsicologosService } from './psicologos.service';
 
-import { Psicologo }    from './entities/psicologos.entity';
-import { Account }      from '../auth/entities/account.entity';
-import { Cita }         from '../citas/entities/citas.entity';
-import { Paciente }     from '../pacientes/entities/paciente.entity';
+import { Psicologo } from './entities/psicologos.entity';
+import { Account } from '../auth/entities/account.entity';
+import { Cita } from '../citas/entities/citas.entity';
+import { Paciente } from '../pacientes/entities/paciente.entity';
+import { Disponibilidad } from '../disponibilidad/entity/disponibilidad.entity'; // ✅ FALTA ESTO
 
-import { AuthModule }             from '../auth/auth.module';
-import { CertificacionesModule }  from '../certificaciones/certificaciones.module';
-import { CitasModule }     from '../citas/citas.module'; 
+import { AuthModule } from '../auth/auth.module';
+import { CertificacionesModule } from '../certificaciones/certificaciones.module';
+import { CitasModule } from '../citas/citas.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Psicologo, Account, Cita, Paciente]),
-
-    // IMPORTAR los módulos que proveen AuthService y CertificacionesService
+    TypeOrmModule.forFeature([
+      Psicologo,
+      Account,
+      Cita,
+      Paciente,
+      Disponibilidad, // ✅ AGREGA ESTO
+    ]),
     AuthModule,
     CertificacionesModule,
-    forwardRef(() => CitasModule), 
+    forwardRef(() => CitasModule),
   ],
   controllers: [PsicologosController],
-  providers:   [PsicologosService],
+  providers: [PsicologosService],
   exports: [PsicologosService],
 })
 export class PsicologosModule {}
