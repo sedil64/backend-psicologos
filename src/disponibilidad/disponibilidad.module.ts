8148edc6bb9100532a1c95b15ad5c65ad127fb46
sedil64 <dilan6465@gmail.com>
2025-07-19 22:59:21 -0500

    inclusion disponibilidad

--- src/disponibilidad/disponibilidad.module.ts
+++ src/disponibilidad/disponibilidad.module.ts
@@ -1,13 +1,18 @@
+// src/disponibilidad/disponibilidad.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { Disponibilidad } from './entity/disponibilidad.entity';
 import { DisponibilidadController } from './disponibilidad.controller';
 import { DisponibilidadService } from './disponibilidad.service';
+import { Psicologo } from '../psicologos/entities/psicologos.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Disponibilidad])],
-  controllers: [DisponibilidadController],   // AGREGAR controlador
-  providers: [DisponibilidadService],       // AGREGAR servicio
-  exports: [TypeOrmModule],                  // puedes dejar o quitar exports según uso
+  imports: [
+    TypeOrmModule.forFeature([Disponibilidad, Psicologo]),
+  ],
+  controllers: [DisponibilidadController],
+  providers: [DisponibilidadService],
+  exports: [DisponibilidadService],
 })
 export class DisponibilidadModule {}
