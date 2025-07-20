ce78b2f1ef21fe18798b77fc3236086d0479cae0
sedil64 <dilan6465@gmail.com>
2025-07-19 23:00:35 -0500

    inclusion disponibilidad

--- src/disponibilidad/disponibilidad.module.ts
+++ src/disponibilidad/disponibilidad.module.ts
@@ -1,18 +1,25 @@
 // src/disponibilidad/disponibilidad.module.ts
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule }        from '@nestjs/typeorm';
 
-import { Disponibilidad } from './entity/disponibilidad.entity';
+import { Disponibilidad }       from './entity/disponibilidad.entity';
+import { DisponibilidadService }from './disponibilidad.service';
 import { DisponibilidadController } from './disponibilidad.controller';
-import { DisponibilidadService } from './disponibilidad.service';
-import { Psicologo } from '../psicologos/entities/psicologos.entity';
+
+// Importa también la entidad Psicologo
+import { Psicologo }            from '../psicologos/entities/psicologos.entity';
+// (opcional) para romper ciclos si PsicologosModule importa DisponibilidadModule
+import { PsicologosModule }     from '../psicologos/psicologos.module';
 
 @Module({
   imports: [
+    // Registras ambos repositorios aquí
     TypeOrmModule.forFeature([Disponibilidad, Psicologo]),
+    // Si PsicologosModule importa DisponibilidadModule también, rompe el ciclo:
+    forwardRef(() => PsicologosModule),
   ],
   controllers: [DisponibilidadController],
-  providers: [DisponibilidadService],
-  exports: [DisponibilidadService],
+  providers:   [DisponibilidadService],
+  exports:     [DisponibilidadService],
 })
 export class DisponibilidadModule {}
